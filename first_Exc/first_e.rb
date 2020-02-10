6df01476e8f8ac94d8fdf4a73506031d0a182175
Krenar123 <krenardardhishta205@gmail.com>
2020-02-10 16:55:37 +0100

    The first exercise done..

---
+++ first_Exc/first_e.rb
@@ -0,0 +1,25 @@
+#Make your script print another line.
+#Make your script print only one of the lines.
+#Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+# 1.) Make your script print another line
+puts("Just printing another line...")
+
+# 2.) Make your scipt print one of the lines..
+
+#puts("This one will not be printed")
+puts("This one yes...")
+#or the other way because i couldnt understand very well
+a=true
+if a==true
+    puts("This one yes...")
+else
+    puts("This one will not be printed")
+end
+
+# 3.) Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+#puts("This character makes the line "unexecutable" -this line will not execute because its comment")
+
+
+
