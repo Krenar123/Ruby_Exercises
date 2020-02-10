802fa8c483dcefb48c401d87fde1b18963bff647
Krenar123 <krenardardhishta205@gmail.com>
2020-02-10 17:01:24 +0100

    Done with sec_e

---
+++ first_Exc/sec_e.rb
@@ -0,0 +1,10 @@
+# We use comments to comment our code
+# just to know what we have done and not mess up our code
+
+puts("Just testing..") #this code prints a string "Just testing.."
+
+#Also we can make a line to a comment, and the code will be disabled 
+# puts "This wont run"
+
+#This one will run
+puts("Yes for sure..")
